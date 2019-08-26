61428e33ea0891ab0ce9083d459d61ca5daad0fc
liwh9 <liwh9@lenovo.com>
2019-08-26 14:21:41 +0800

    first

---
+++ src/main/java/automation/ui/App.java
@@ -0,0 +1,13 @@
+package automation.ui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
